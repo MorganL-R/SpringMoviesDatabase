3091555b79201aee5a42f71350d963370edf14f9
Morgan Lawley-Robinson <morgan.a.lawley-robinson@capgemini.com>
2021-09-02 17:20:13 +0100

    Added testRreadMovies

--- src/test/java/com/morgan/repo/MovieRepoReadTest.java
+++ src/test/java/com/morgan/repo/MovieRepoReadTest.java
@@ -1,20 +1,28 @@
 package com.morgan.repo;
 
+
 import com.morgan.model.Actor;
 import com.morgan.model.Movie;
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 @AutoConfigureTestDatabase
 @SpringBootTest
-public class MovieRepoTest {
+public class MovieRepoReadTest {
     @Autowired
     private MovieRepo movieRepo;
 
-    @Test
-    public void testWriteMovie() {
+
+    public void insertMovies() {
         Movie movie = new Movie();
         movie.setTitle("Spiral");
         movie.setGenre("Thriller");
@@ -26,21 +34,27 @@ public class MovieRepoTest {
         actor.setGender("Male");
         movie.addActor(actor);
         movieRepo.save(movie);
-    }
 
-    @Test
-    public void testWriteMovie2(){
-        Movie movie = new Movie();
+
+        Movie movie2 = new Movie();
         movie.setTitle("The Green Mile");
         movie.setGenre("Drama");
         movie.setRuntime(189);
         movie.setRating("18");
-        Actor actor = new Actor();
+        Actor actor2 = new Actor();
         actor.setName("Tom Hanks");
         actor.setAge(65);
         actor.setGender("Male");
         movie.addActor(actor);
-        movieRepo.save(movie);
+        movieRepo.save(movie2);
+    }
+
+    @Test
+    public void testReadMovies() {
+        insertMovies();
+        List <Movie> movies = movieRepo.findAll();
+        assertFalse(movies.isEmpty());
+        assertEquals(2, movies.size());
     }
 
 }
