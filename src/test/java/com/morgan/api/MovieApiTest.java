1b0d90d9ce4fabd23252121b3ae9f0c8a597cec3
Morgan Lawley-Robinson <morgan.a.lawley-robinson@capgemini.com>
2021-09-21 13:23:20 +0100

    Added API tests

--- src/test/java/com/morgan/api/MovieApiTest.java
+++ src/test/java/com/morgan/api/MovieApiTest.java
@@ -1,5 +1,6 @@
 package com.morgan.api;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.text.Format;
+
+import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,6 +23,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 public class MovieApiTest {
     @Autowired
     private MockMvc mockMvc;
+    private String movieJson = "{\"title\":\"Spiral\",\"genre\":\"Thriller\",\"runtime\":93,\"rating\":\"18\",\"actors\":[{\"name\":\"Chris Rock\",\"age\":53,\"gender\":\"male\"}]}";
+    private String updatedMovieJson = "{\"id\":%s,\"title\":\"John\",\"genre\":\"Thriller\",\"runtime\":93,\"rating\":\"18\",\"actors\":[{\"id\":2,\"name\":\"Chris Rock\",\"age\":53,\"gender\":\"male\"}]}";
+
 
     @Test
     public void testList() throws Exception {
@@ -29,20 +35,8 @@ public class MovieApiTest {
     }
 
     public Long createMovie() throws Exception {
-        return Long.parseLong(mockMvc.perform(post("/movies").content("{\n" +
-                "\t\"title\": \"Spiral\",\n" +
-                "\t\"genre\": \"Thriller\",\n" +
-                "\t\"runtime\": 93,\n" +
-                "\t\"rating\": \"18\",\n" +
-                "\t\n" +
-                "\t\"actors\": [\n" +
-                "\t\t{\n" +
-                "\t\t\t\"name\": \"Chris Rock\",\n" +
-                "\t\t\t\"age\": 53,\n" +
-                "\t\t\t\"gender\": \"male\"\n" +
-                "\t\t}\n" +
-                "\t\t]\n" +
-                "}").contentType("application/json")).andReturn().getResponse().getContentAsString());
+        return Long.parseLong(mockMvc.perform(post("/movies")
+                .content(movieJson).contentType("application/json")).andReturn().getResponse().getContentAsString());
     }
 
     @Test
@@ -50,6 +44,21 @@ public class MovieApiTest {
         Long id = createMovie();
         MvcResult result = mockMvc.perform(get("/movies/{id}", id).accept("application/json")).andReturn();
         assertEquals(200, result.getResponse().getStatus());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        Long id = createMovie();
+        updateMovie(id);
+        MvcResult result = mockMvc.perform(get("/movies/{id}", id)).andReturn();
+        assertEquals(200, result.getResponse().getStatus());
+        String json = result.getResponse().getContentAsString();
+        assertEquals(format(updatedMovieJson, id), json);
+        System.out.print(json);
+    }
+    public void updateMovie(Long id) throws Exception {
+            mockMvc.perform(put("/movies")
+                    .content(format(updatedMovieJson, id)).contentType("application/json")).andReturn().getResponse().getContentAsString();
     }
 
 
